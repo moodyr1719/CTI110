3d5b3b62b575b2534d43177ed9b5fea0adde0b37
moodyr1719 <125482655+moodyr1719@users.noreply.github.com>
2023-04-18 01:31:18 -0400

    Add files via upload

---
+++ P4LAB1a_MoodyRina.py
@@ -0,0 +1,36 @@
+import turtle
+wn = turtle.Screen()
+
+rina = turtle.Turtle()
+wn.bgcolor("pink")
+rina.color("red")
+rina.pensize(4)
+rina.shape("classic")
+
+rina.left(90)
+rina.forward(110)
+rina.right(90)
+rina.forward(35)
+for A in range(5):
+    rina.right(30)
+    rina.forward(15)
+
+rina.right(28)
+rina.forward(35)
+rina.left(135)
+rina.forward(75)
+
+rina.penup()
+rina.left(43)
+rina.forward(40)
+rina.pendown()
+
+rina.left(80)
+rina.forward(110)
+rina.right(150)
+rina.forward(80)
+rina.left(140)
+rina.forward(80)
+rina.right(150)
+rina.forward(110)
+
