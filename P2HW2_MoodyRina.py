2ff5259177ed0f5c9f15a8c2e770670223647ba3
moodyr1719 <125482655+moodyr1719@users.noreply.github.com>
2023-04-30 17:46:19 -0400

    Add files via upload

---
+++ P2HW2_MoodyRina.py
@@ -0,0 +1,34 @@
+# CTI-110
+# P2HW2 - List
+# Rina Moody
+# March 13, 2023
+# Program calculates the grades
+#
+##********Psudocode********
+#Ask user "Enter your grade for Module 1: "
+#Input Module 1
+#Ask user "Enter your grade for Module 2: "
+#Input Module 2
+#Ask user "Enter your grade for Module 3: "
+#Input Module 3
+#Ask user "Enter your grade for Module 4: "
+#Input Module 4
+#Ask user "Enter your grade for Module 5: "
+#Input Module 5
+#Ask user "Enter your grade for Module 6: "
+#Input Module 6
+#Set Lowest Grade =
+#Set Highest Grade =
+#Set Sum of Grades =
+#Set Average =
+#Display Lowest Grade:
+#Display Highest Grade:
+#Display Sum of Grades:
+#Display Average:
+
+Module1 = float(input('Enter grade for Module 1: '))
+Module2 = float(input('Enter grade for Module 2: '))
+Module3 = float(input('Enter grade for Module 3: '))
+Module4 = float(input('Enter grade for Module 4: '))
+Module5 = float(input('Enter grade for Module 5: '))
+Module6 = float(input('Enter grade for Module 6: '))
